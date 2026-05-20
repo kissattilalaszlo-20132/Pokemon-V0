a13a561c79efc01611818e15693fd1630fc15d37
Kiss Attila <kissattilalaszlo-20132@taszi.hu>
2026-05-20 20:07:40 +0200

    added info summary

--- pokemon.js
+++ pokemon.js
@@ -1,13 +1,75 @@
 import { APIfetch } from "./main.js";
 
+function infoBuilder(name, value) {
+    let line = document.createElement("div");
+    
+    let lineName = document.createElement("p");
+    lineName.textContent = name + ":  ";
+
+    let lineValue = document.createElement("p");
+    lineValue.textContent = value;
+
+    line.appendChild(lineName);
+    line.appendChild(lineValue);
+
+    return line;
+}
+
 // for pokemon submenu
-export function pokemonSummary(item){
+export async function pokemonSummary(item){
     let summary = document.createElement("div");
     summary.classList.add("summary");
     
+    // get general info 
+    const species = await APIfetch(item.species.url);
+
+    let info = document.createElement("div");
+    info.classList.add("info");
+
+    info.appendChild(infoBuilder("color", species.color.name));
+    info.appendChild(infoBuilder("habitat", species.habitat.name));
+
+    let shape = document.createElement("p");
+    shape.textContent = species.shape.name;
+
+    info.appendChild(shape);
+
+    let genderRate = document.createElement("p");
+    switch (species.gender_rate) { // ♀️ ♂️
+        case 0:
+            genderRate.textContent = "cannot breed";
+            break;
+        case 1:
+            genderRate.textContent = "1♀️ : 7♂️";
+            break;
+        case 2:
+            genderRate.textContent = "1♀️ : 3♂️";
+            break;
+        case 4:
+            genderRate.textContent = "1♀️ : 1♂️";
+            break;
+        case 6:
+            genderRate.textContent = "3♀️ : 1♂️";
+            break;
+        case 7:
+            genderRate.textContent = "7♀️ : 1♂️";
+            break;
+        case 8:
+            genderRate.textContent = "female only";
+            break;
+        case -1:
+            genderRate.textContent = "gender unknown";
+            break;
+        default:
+            genderRate.textContent = "not documented";
+            break;
+    }
+    info.appendChild(genderRate);
+
+    summary.appendChild(info);
+
     // sprite
     let sprite = document.createElement("img");
-    sprite.classList.add(("pokemonSprite", "sprite"));
     sprite.src = item.sprites.other.showdown.front_default;
     summary.appendChild(sprite);
 
